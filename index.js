71f25f0143b8a9106164f8f91b3b48a813252116
dsabovic1 <dsabovic1@etf.unsa.ba>
2019-05-05 14:15:24 +0200

    Dodan api za tabelu studenti

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
-const express = require('express')
-const app = express()
-const port = 31906
+const express = require('express');
+// init express
+const app = express();
+const port = /*process.env.port ||*/ 31906;
 
-app.get('/', (req, res) => res.send('Hello World from FOX!'))
+//npm run dev
+app.get('/', (req, res) => res.send('Hello World from FOX!'));
 
+//Return info for table Studenti
+const studenti = [
+    {
+        index: 12345,
+        imePrezime: 'Neko Nekić',
+        prisustvo: 10,
+        zadace: 10,
+        ispiti : [
+            {naziv: 'I parcijalni ispit', bodovi: '15'},
+            {naziv: 'II parcijalni ispit', bodovi: '15'},
+            {naziv: 'Usmeni ispit', bodovi: '20'},
+        ],
+        ukupno: 70,
+        ocjena: 7
+    }
+];
+
+
+app.get('/api/fox/tabelaStudenti', (req, res) => {
+    res.json(studenti);
+});
+
+
+
+//Listen on port
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
