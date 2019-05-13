2820472f189d0187019068ff50f1e59b2b38d835
dsabovic1 <dsabovic1@etf.unsa.ba>
2019-05-13 22:32:36 +0200

    Dodan mikroservis za dodavanje zahtjeva

--- services/ZahtjeviZavrsniRoute.js
+++ services/ZahtjeviZavrsniRoute.js
@@ -27,7 +27,27 @@ zahtjeviZavrsniRouter.get('/zahtjevi/:idProfesora', (req,res)=> {
             err
         }));
         else res.send(zahtjevi);
-    })
+    });
+});
+
+zahtjeviZavrsniRouter.post('/dodajZahtjev', (req,res) => {
+    let postBody = req.body;
+    res.setHeader('Content-Type', 'application/json');
+
+    let ispravniParametri = zahtjeviZavrsniUtils.provjeraParametaraDodajZahtjev(postBody);
+    if (!ispravniParametri) res.send(JSON.stringify({
+        message: 'Neispravni parametri unutar body-a! Ocekivani format formatu [idTema, idStudent, idProfesor, odobreno]'
+    }))
+    else {
+        zahtjeviZavrsniUtils.dodajZahtjev(postBody, (err,tema)=> {
+            if (err) res.send(JSON.stringify({
+                message: "Neispravni id-evi!"
+            }));
+            else res.send(JSON.stringify({
+                message: "Uspjesno dodan novi zahtjev!"
+            }));
+        });
+    } 
 })
 
 module.exports = zahtjeviZavrsniRouter;
--- utils/zahtjeviZavrsniUtils.js
+++ utils/zahtjeviZavrsniUtils.js
@@ -27,8 +27,54 @@ const getZahtjeviZavrsni = (idProfesora, callback) => {
     });
 }
 
+const provjeraParametaraDodajZahtjev = (postBody) => {
+    if(!postBody['idTema'] || !postBody['idStudent'] || !postBody['idProfesor'] || !postBody['odobreno']) return false;
+    return true;
+}
+
+const dodajZahtjev = (postBody, callback) => {
+    let noviZahtjev = {
+        idTema: postBody['idTema'],
+        idStudent: postBody['idStudent'],
+        idProfesor: postBody['idProfesor'],
+        odobreno: postBody['odobreno']
+    }
+    //Provjera da li postoje tema, student i profesor
+    db.TemeZavrsnih.findOne({
+        where: {
+            id: postBody['idTema'] 
+        }
+    }).then((tema) => {
+        if (!tema) callback(true); //Greska
+        else {
+            db.Korisnik.findOne({
+                where: {
+                    id: postBody['idProfesor']
+                }
+            }).then((profesor) => {
+                if(!profesor) callback(true); //Greska
+                else {
+                    db.Korisnik.findOne({
+                        where: {
+                            id: postBody['idStudent']
+                        }
+                    }).then((student) => {
+                        if(!student) callback(true); //Greska
+                        else {
+                            db.ZahtjeviZavrsni.create(noviZahtjev).then((novi) => {
+                                if (!novi) callback(true); //Greska
+                                else callback(null, novi); })
+                        }
+                    });
+                }
+            });
+        }
+    })
+}
+
 module.exports = {
     odobriZahtjeviZavrsni,
     getZahtjeviZavrsni,
-    //addZahtjeviZavrsni
+    provjeraParametaraDodajZahtjev,
+    dodajZahtjev
 }
