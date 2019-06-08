4d5a7b05e215065ee81b19acdc07f0b1858a5a4a
dsabovic1 <dsabovic1@etf.unsa.ba>
2019-06-08 21:21:51 +0200

    Ispravljen api za početnu stranicu

--- index.js
+++ index.js
@@ -144,50 +144,6 @@ app.get('/api/fox/tabelaStudenti/ispiti', cors(), (req, res) => {
     res.json(ispiti);
 });*/
 
-//APIji za početnu stranicu
-
-const predmeti = [
-    {
-        id: 1,
-        naziv: "Tehnike programiranja"
-    },
-    {
-        id: 2,
-        naziv: "Numerički algoritmi"
-    },
-    {
-        id: 3,
-        naziv: "Diskretna matematika"
-    }
-]
-
-const grupe = [
-    {
-        id: 1,
-        naziv: "Grupa 1"
-    },
-    {
-        id: 2,
-        naziv: "Grupa 2"
-    },
-    {
-        id:3,
-        naziv: "Grupa 3"
-    }
-]
-
-/*app.get('/api/fox/predmeti/:idKorisnika', cors(), (req, res) => {
-    //'/api/fox/tabelaStudenti?_limit=100'
-    console.log(req.params);
-    res.json(predmeti);
-});
-
-app.get('/api/fox/grupe/:idPredmeta', cors(), (req, res) => {
-    //'/api/fox/tabelaStudenti?_limit=100'
-    console.log(req.params);
-    res.json(grupe);
-});*/
-
 
 //endpointi za ocjene
 
