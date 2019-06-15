d4d4912f854514da8f68384544352c8999f093be
dsabovic1 <dsabovic1@etf.unsa.ba>
2019-06-15 07:47:24 +0200

    Rješenje za cors

--- index.js
+++ index.js
@@ -36,10 +36,10 @@ db.sequelize.sync()
 app.get('/', (req, res) => res.send('Hello World from FOX!'));
 
 
-app.use('/*', (req, res, next) => {
+app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*'); //Posebno za samo nas frontend?!
     res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    // res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
 //---------------Mikroservisi--------------------------------------------------------------------
